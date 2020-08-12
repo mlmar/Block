47e635b4764ef1606a41157810c1f310ca1c662e
mlmar <marcloussm@gmail.com>
2020-08-12 04:14:55 -0700

    updated settings text

--- text/text.js
+++ text/text.js
@@ -8,7 +8,9 @@ general += "please try to make it to level 10.";
 general += "\n\n";
 
 general +=
-  "i'm trying to make this work on mobile but it's hard."
+  "mobile (kind of) works but it's slower. if you're on ios you need to use the 'ADD TO HOMESCREEN' option"
+general +=
+  "i have no idea how this runs on android."
 
 scoreList = [
   "JOHNNY: 351 <----- beat this",
